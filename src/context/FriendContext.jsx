2bac913c405c73a082da817fd9d9e946fc86f72a
Ashik2630 <ashik426370@gmail.com>
2026-04-17 01:25:11 +0600

    Update index.css and firendContext file Update

--- src/context/FriendContext.jsx
+++ src/context/FriendContext.jsx
@@ -15,20 +15,6 @@ const FriendProvider = ({ children }) => {
 
 //   Call-handlerButton
   const handleCallButton = (currentCall) => {
-
-    // Text exist error
-    const existsInText = text.find((c) => c.id === currentCall.id);
-    if(existsInText){
-        toast.error('The Text is already exist in list');
-        return;
-    }
-    // Video exist error
-    const existsInVideo = video.find((c) => c.id === currentCall.id);
-    if(existsInVideo){
-        toast.error('The Text is already exist in list');
-        return;
-    }
-
     // Call exist
     const isExistCall = call.find((c) => c.id === currentCall.id);
     if (isExistCall) {
@@ -40,20 +26,6 @@ const FriendProvider = ({ children }) => {
   };
 //   Text-handlerButton
   const handleTextButton = (currentText) => {
-
-    // text exist error
-    const existsInCall = call.find((c) => c.id === currentText.id);
-    if(existsInCall){
-        toast.error('The Call is already exist in list');
-        return;
-    }
-    // text exist error
-    const existsInVideo = video.find((c) => c.id === currentText.id);
-    if(existsInVideo){
-        toast.error('The Call is already exist in list');
-        return;
-    }
-
     // Text exist
     const isExistText = text.find((t) => t.id === currentText.id);
     if (isExistText) {
@@ -65,21 +37,6 @@ const FriendProvider = ({ children }) => {
   };
 //   Video-handlerButton
   const handleVideoButton = (currentVideo) => {
-
-    // call exist error
-    const existsInCall = call.find((c) => c.id === currentVideo.id);
-    if(existsInCall){
-        toast.error('The Call is already exist in list');
-        return;
-    }
-
-    // text exist error
-     const existsInText = text.find((c) => c.id === currentVideo.id);
-    if(existsInText){
-        toast.error('The Text is already exist in list');
-        return;
-    }
-
     // video exist
     const isExistText = video.find((v) => v.id === currentVideo.id);
     if (isExistText) {
